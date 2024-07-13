cb5c95b309e776073c6e0c91cbb995a3fcc64695
Adel Fayed <fayedadel627@gmail.com>
2024-07-14 00:34:27 +0300

    project student_entry

---
+++ Students_Entry/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Students_Entry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Entre the arry size : ");
+            int input = int.Parse(Console.ReadLine());
+            students[] student = new students[input];
+            for (int i = 0; i < input; i++)
+            {
+                student[i] = new students();
+                Console.Write($"Entre the name of student {i + 1}: ");
+                student[i].name = Console.ReadLine();
+                Console.Write($"Entre the age of the student {i + 1}: ");
+                student[i].age = int.Parse(Console.ReadLine());
+                Console.Write($"Entre the id of student {i + 1}: ");
+                student[i].id = int.Parse(Console.ReadLine());
+            }
+            foreach (var s in student)
+            {
+                Console.WriteLine(s.name);
+                Console.WriteLine(s.age);
+                Console.WriteLine(s.id);
+                Console.WriteLine("----------------------------------------------------");
+            }
+        }
+    }
+}
---
+++ Students_Entry/students.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Students_Entry
+{
+    internal class students
+    {
+        public string name;
+        public int id;
+        public int age;
+    }
+}
